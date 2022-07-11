6c1dd306c8440f3ab8e86401461ace44c7d6397c
Kevin <kyturr@gmail.com>
2022-07-10 19:11:39 -0700

    Deliverable 2 Query and CSV

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -33,3 +33,22 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY count DESC;
+
+
+-- Deliverable 2: Mentorship Eligible Table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no=de.emp_no)
+INNER JOIN titles AS ti
+ON (e.emp_no=ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no, to_date DESC;
